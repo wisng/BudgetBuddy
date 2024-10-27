7017eca233993de61013d180d06d77010be3615d
Pancho Fernandez <panchofern@gmail.com>
2024-10-26 23:45:05 -0400

    initial template

---
+++ frontend/src/screens/Transactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Transactions = () => {
+  return (
+    <div>
+      <h1>Transactions Page</h1>
+      <p>Learn more Transactions us on this page.</p>
+    </div>
+  );
+};
+
+export default Transactions;
