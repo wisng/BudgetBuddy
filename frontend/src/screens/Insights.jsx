834a840af93d4b078588431b4f5c9631d54c56cc
humairraa <humairraadeeb@gmail.com>
2024-11-27 21:16:24 -0500

    commented unnecesary code, re-arranged chart components, added chart titles

--- frontend/src/screens/Insights.jsx
+++ frontend/src/screens/Insights.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { Typography, TextField, Button, Box, Link, Paper, Grid2 as Grid } from "@mui/material";
 import MonthPicker from "../components/MonthPicker";
 import { LineChart, BarChart, PieChart } from '@mui/x-charts';
+import { alpha, useTheme } from "@mui/material";
+import { useDrawingArea } from '@mui/x-charts/hooks';
+import { styled } from '@mui/material/styles';
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const spendingHabits = {
+  "Groceries": 150,
+  "Entertainment": 50,
+  "Gas/Automobile": 200,
+  "Recurring Payments": 50
+}
+
+const uData = [4000, 3000, 2000, 6780, 7890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
 ];
 export const dataset = [
   {
@@ -73,46 +83,115 @@ const chartSetting = {
   height: 400,
 };
 
+const StyledText = styled('text')(({ theme }) => ({
+  fill: theme.palette.text.primary,
+  textAnchor: 'middle',
+  dominantBaseline: 'central',
+  fontSize: 20,
+}));
+
+function PieCenterLabel({ children }) {
+  const { width, height, left, top } = useDrawingArea();
+  return (
+    <StyledText x={left + width / 2} y={top + height / 2} sx={{fontSize:"45px", fontWeight:"600"}}>
+      {children}
+    </StyledText>
+  );
+}
+
+
+const Colorswitch = () => {
+  const { top, height, bottom } = useDrawingArea();
+  const svgHeight = top + bottom + height;
+
+  return (
+      <>
+          <defs>
+              <linearGradient id="paint0_linear_45_2" x1="300.25" y1="46.9999" x2="300.25" y2={`${svgHeight}px`} gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#dad4f9" stop-opacity="0.4" />
+                  <stop offset="1" stop-color="#2F4CDD" stop-opacity="0" />
+              </linearGradient>
+          </defs>
+
+          <defs>
+              <linearGradient id="paint0_linear_45_3" x1="299.498" y1="-4.28272" x2="299.498" y2={`${svgHeight}px`} gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#B519EC" stop-opacity="0.4" />
+                  <stop offset="1" stop-color="#B519EC" stop-opacity="0" />
+              </linearGradient>
+          </defs>
+      </>
+  )
+}
 
 const Insights = ({ budget, goals }) => {
+  const theme = useTheme();
   return (
     <Box>
-      <Grid container spacing={2}>
+      {/* <Grid container spacing={2}>
         <Grid size={2} sx={{}}></Grid>
         <Grid size={8}>
           <MonthPicker startDate={budget.creationDate} handleSubmit={() => alert("HELLO")} />
         </Grid>
         <Grid size={2} sx={{}}></Grid>
-      </Grid>
-
+      </Grid> */}
       <Grid
         container
-        sx={{padding:"2rem", justifySelf:"center"}}>
+        spacing={4}
+      >
+        <Grid size={2}></Grid>
+        <Grid
+          size={7}
+        > 
         <Paper 
-          sx={{borderRadius:"30px", padding:"2rem"}} 
+          sx={{borderRadius:"30px", padding:"2rem", marginTop:"0.5rem", marginBottom:"2rem"}} 
           elevation={2} 
           square={false}>
+          <Typography variant="h5" sx={{position:"relative", top:"0rem", alignSelf:"flex-start", fontWeight:"600", textAlign:"left", marginLeft:"0.,5rem"}}>
+            Transactions
+          <Grid sx={{position:"relative", top:"-2.5rem", left:"12rem", width:"50%"}}>
+            <MonthPicker startDate={budget.creationDate} handleSubmit={() => alert("HELLO")} />
+          </Grid>
+          </Typography>
           <LineChart
+            leftAxis={{
+              disableLine: true,
+              disableTicks: true
+            }}
+            bottomAxis={{
+              disableTicks: true
+            }}
             sx={{backgroundColor:"white", borderRadius:"50px"}}
-            width={800}
             height={300}
             series={[
-              { data: pData, label: 'Expenses' },
-              { data: uData, label: 'uv' },
+              { data: pData, area: true, color:"#dad4f9", label:"Expenses"},
+              { data: uData,  color:"#8c75ff", label: "Income"},
             ]}
             xAxis={[{ scaleType: 'point', data: xLabels }]}
+            slotProps={{
+              legend: {
+                  direction: 'row',
+                  position: { vertical: 'top', horizontal: 'right' },
+                  itemGap: 62,
+              },
+          }}
           />
+          <Colorswitch />
           </Paper>
+          </Grid>
+          <Grid size={3}></Grid>
       </Grid>
       <Grid container spacing={1} sx={{marginBottom:"25px"}}>
         <Grid size={2}></Grid>
         <Grid size={3}>
-          <Paper 
+          <Paper
           sx={{borderRadius:"30px"}}  
           elevation={2} 
           square={false}>
+          <Typography variant="h5" sx={{position:"relative", top:"1rem", alignSelf:"flex-start", fontWeight:"600", textAlign:"left", marginLeft:"3rem"}}>
+            Daily Spending
+          </Typography>
            <BarChart
-              sx={{}}
+              sx={{marginRight:"1.5em"}}
               leftAxis={null}
               bottomAxis={{
                 disableLine: true,
@@ -133,25 +212,38 @@ const Insights = ({ budget, goals }) => {
           sx={{borderRadius:"30px"}}  
           elevation={1}
           square={false}>
+            <Typography variant="h5" sx={{position:"relative", top:"1rem", alignSelf:"flex-start", fontWeight:"600", textAlign:"left", marginLeft:"3rem"}}>
+              Spending Habits
+            </Typography>
           <PieChart
-              sx={{ top: "15%", left: "10%" }}
-              colors={["#FE6C6C", "#FEBD38"]}
-              series={[
-                {
-                  startAngle: 0,
-                  endAngle: 360,
-                  innerRadius: 100,
-                  outerRadius: 180,
-                  paddingAngle: 3,
-                  cornerRadius: 10,
-                  cx: "80%",
-                  cy: "50%",
-                  data: [{ value: budget.financialHealthScore }, { value: 100 - budget.financialHealthScore }],
-                },
-              ]}
-              height={400}
-              width={400}
-              />
+            sx={{marginLeft:"65px"}}
+            colors={["#FE6C6C", "#FEBD38"]}
+            series={[
+              {
+                startAngle: 0,
+                endAngle: 360,
+                innerRadius: 100,
+                outerRadius: 180,
+                paddingAngle: 3,
+                cornerRadius: 10,
+                highlightScope: { fade: 'global', highlight: 'item' },
+                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                data: [
+                  { value: spendingHabits.Groceries, label: "Groceries", color:"#FFD26A" } , 
+                  { value: spendingHabits.Entertainment, label: "Entertainment", color:"#73CFFC" },
+                  { value: spendingHabits["Gas/Automobile"], label: "Gas/Automobile", color:"#FE63BF" },
+                  { value: spendingHabits["Recurring Payments"], label: "Recurring Payments", color:"#FD5357" }
+                ],
+              },
+            ]}
+            height={400}
+            width={500}
+            slotProps={{
+              legend: { hidden: true },
+            }}
+          >
+          <PieCenterLabel>$450</PieCenterLabel>
+          </PieChart>
           </Paper>
         </Grid>
       </Grid>
